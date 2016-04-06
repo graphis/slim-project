40242be8bfc77ee880ac5ebb07efffc4c1b4f8cb
Zsolt Sándor <ezsolt@gmail.com>
2016-04-06 17:30:37 +0200

    initial commit

---
+++ application/bootstrap.php
@@ -0,0 +1,4 @@
+<?php
+
+// http://www.slimframework.com/docs/tutorial/first-app.html
+// https://www.codecourse.com/library/lessons/slim-3-controllers-dependency-injection/controller-basics
---
+++ public/index.php
@@ -0,0 +1,4 @@
+<?php
+
+require dirname(__DIR__) . '/system/autoload.php';
+require dirname(__DIR__) . '/application/bootstrap.php';
