168f1a67be07b5de7ca1f42519e4fc5a153c998c
Zsolt Sándor <ezsolt@gmail.com>
2016-04-06 17:36:51 +0200

    simple test setup

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -2,3 +2,25 @@
 
 // http://www.slimframework.com/docs/tutorial/first-app.html
 // https://www.codecourse.com/library/lessons/slim-3-controllers-dependency-injection/controller-basics
+
+
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+
+
+$app = new \Slim\App;
+
+
+// routes
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+
+
+
+$app->run();
