b909c447d69ea0e3e696f02a24d751a23e08cd6b
Zsolt Sándor <ezsolt@gmail.com>
2016-04-08 22:11:55 +0200

    adding bootstraps

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
 
 require dirname(__DIR__) . '/system/autoload.php';
-require dirname(__DIR__) . '/application/boot/bootstrap.php';
+// require dirname(__DIR__) . '/application/boot/bootstrap-middleware.php';
+require dirname(__DIR__) . '/application/boot/bootstrap-group-middleware.php';
